d4f2ea009442db5ff658737c1faf213aa4ded3f4
Laurens van der Kevie <ljvanderkevie@gmail.com>
2023-08-24 14:56:09 +0200

    Initial Commit

---
+++ Nu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Nu
+//
+//  Created by Laurens on 24/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nu/NuApp.swift
@@ -0,0 +1,17 @@
+//
+//  NuApp.swift
+//  Nu
+//
+//  Created by Laurens on 24/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct NuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
